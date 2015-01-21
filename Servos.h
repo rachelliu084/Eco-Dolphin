41076817c245d544df3766a9f7f2679d17d0a29d
wxzhouqi <wxzhouqi@hotmail.com>
2015-01-21 17:46:37 -0500

    Create Servos.h

---
+++ Servos.h
@@ -0,0 +1,15 @@
+#ifndef _SERVOS_H_
+#define _SERVOS_H_
+
+#include <Arduino.h>
+#include <Servo.h>
+
+#define Servo1_PIN 4
+#define Servo2_PIN 5
+#define Servo3_PIN 6
+#define Servo4_PIN 7
+
+
+void Servo_Angle(int *servo);
+
+#endif
