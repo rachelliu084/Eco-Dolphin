408a57440736b41d63a73c81b7c903e19fc773b9
Stacey Joseph-Ellison <hedstartllc@gmail.com>
2015-01-20 20:24:32 -0500

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,25 @@
+//the global type assignments and header files are kept here
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include "maneuvers.h"
+#include "comms.h"
+#include "modes.h"
+
+#define CC_FULL_SPEED 1400
+#define CC_MED_SPEED 1550
+#define CC_LOW_SPEED 1409
+#define IDLE 1500
+#define CW_FULL_SPEED 1570
+#define CW_MED_SPEED 1550
+#define CW_LOW_SPEED 1530
+#define FS_TURN_RATE 5000/90
+#define MS_TURN_RATE 6000/90
+#define LS_TURN_RATE 7000/90
+#define FS_DIST_RATE 1000/3
+#define MS_DIST_RATE 1000/2
+#define LS_DIST_RATE 1000/1
