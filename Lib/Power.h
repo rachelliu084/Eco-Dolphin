4ae99c0eb2306f76477d6f2b7b519aa59a645612
wxzhouqi <wxzhouqi@hotmail.com>
2015-05-10 17:53:30 -0400

    Update Power.h

--- Lib/Power.h
+++ Lib/Power.h
@@ -3,18 +3,14 @@
 
 #include <Arduino.h>
 
-#define EN_6 25
-#define EN_12 27
-#define EN_3V3 29
-#define RELAY 12
 
-#define ALL_ON 0x0F
-#define ON_25V 0x08
-#define ON_12V 0x04
-#define ON_6V 0x02
-#define ON_3V3 0x01
-#define ALL_OFF 0x00
+#define RELAY 12//pin 12
 
-void POWER_ON(int power);
+#define Thruster_ON	1
+#define Thruster_OFF	0
+
+int Thruster_PWR(int x);
+
+void Power_Init();//Initilize the control pin
 
 #endif
