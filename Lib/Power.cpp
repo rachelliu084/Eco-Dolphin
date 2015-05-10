0b2d5ef51d93e2e6afc64a62c0c3c3aff0db9465
wxzhouqi <wxzhouqi@hotmail.com>
2015-05-10 17:53:17 -0400

    Update Power.cpp

--- Lib/Power.cpp
+++ Lib/Power.cpp
@@ -1,33 +1,21 @@
 #include "Power.h"
 
-
-void POWER_ON(int power)
+void Power_Init()//Initilize the control pin
 {
-  int t = 1;
-  if(t)
-  {
-    pinMode(EN_6,OUTPUT);
-    pinMode(EN_12,OUTPUT);
-    pinMode(EN_3V3,OUTPUT);
-    pinMode(RELAY,OUTPUT);
-    t = 0;
+  pinMode(RELAY,OUTPUT);
+}
+
+
+int Thruster_PWR(int x)//switch of 25V 1=ON, 0=OFF
+{
+  if(x == 1) 
+  { 
+    digitalWrite(RELAY, HIGH); 
+    return 1;
   }
-  if((power & ALL_ON) == ALL_ON) //
+  else 
   {
-    digitalWrite(EN_6, LOW);
-    digitalWrite(EN_12, LOW);
-    digitalWrite(EN_3V3, LOW);
-    digitalWrite(RELAY, HIGH);
-  }
-  if((power & ON_25V) == ON_25V)  digitalWrite(RELAY, HIGH);    
-  if((power & ON_12V) == ON_12V)  digitalWrite(EN_12, LOW);
-  if((power & ON_6V) == ON_6V)    digitalWrite(EN_6, LOW);
-  if((power & ON_3V3) == ON_3V3)  digitalWrite(EN_3V3, LOW);
-  if((power & ALL_OFF) == ALL_OFF)
-  {
-    digitalWrite(EN_6, HIGH);
-    digitalWrite(EN_12, HIGH);
-    digitalWrite(EN_3V3, HIGH);
-    digitalWrite(RELAY, LOW); 
+    digitalWrite(RELAY, LOW);
+    return 0;
   }
 }
