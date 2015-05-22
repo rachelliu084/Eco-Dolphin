953f7df7f7745f9327c23ae09fb39a474db12547
ptang0917 <ptang24@gmail.com>
2015-05-22 14:04:58 -0400

    Update YED_05032015.ino

--- YED_05032015/YED_05032015.ino
+++ YED_05032015/YED_05032015.ino
@@ -6,13 +6,13 @@
 #include "Thruster.h"
 #include "Raspberry.h"
 #include <Servo.h>
-#include <String.h>
+#include <string.h>
 
 /*---------------------Status Word-----------------------*/
 char Data_Raspberry[Raspberry_Data_Width];
 char Data_Sonar[Sonar_Data_Width];
 char x[8], y[8], z[8];
-int TH[5] = {1500, 1500, 1500 ,1500 , 0};// Last one TH[4] is time, 0-99s
+int TH[4] = {1500, 1500, 1500 ,1500};// Last one TH[4] is time, 0-99s
 
 /*---------------------Status Word-----------------------*/
 int Th_PWR = 0;
@@ -32,51 +32,55 @@ void setup()
 //  Thruster_Init();
 }
 
-void loop()
-{
+void loop() {
   strcpy(Data_Raspberry, "");// clear command
-  Raspberry_RX(Data_Raspberry);// wait until get the next command
-  if(Data_Raspberry == "PowerOn")
-  {
-    strcpy(Data_Raspberry, "");
-    if(Th_PWR == 0) { Th_PWR = Thruster_PWR(Thruster_ON); Thruster_Init(); Buzzer_3x500ms(); }
-    Thruster_Stop();
-    Raspberry_TX(Ready);
-    Raspberry_RX(Data_Raspberry);
-    if(Data_Raspberry == "IMU")  { 
+  Raspberry_RX(Data_Raspberry);
+  while(Data_Raspberry != 'IMUSet') { //beginning of while loop
+    
+  if(strcmp(Data_Raspberry, "IMU")==0)  { 
       IMU_Data(Accel, x, y, z); Raspberry_TX(x+y+z); 
       IMU_Data(Gyro, x, y, z); Raspberry_TX(x+y+z);
     }
     else {
-       Raspberry_TX("WRONG DATA");
+       Raspberry_TX("Wrong Command!");
     }
 
-  else if(Data_Raspberry == "Motion")
+  else if(strcmp(Data_Raspberry,"Motion")==0)
       {
         strcpy(Data_Raspberry, "");
         if(Th_PWR == 0) { Th_PWR = Thruster_PWR(Thruster_ON); Thruster_Init(); Buzzer_3x500ms(); }
         Raspberry_TX(Ready);
         Raspberry_RX(Data_Raspberry);
-        if(Data_Raspberry == "IMU")  { 
+        if(strcmp(Data_Raspberry, "IMU")==0)  { 
           IMU_Data(Accel, x, y, z); Raspberry_TX(x+y+z); 
           IMU_Data(Gyro, x, y, z); Raspberry_TX(x+y+z); 
-    }
-       /* else if(Data_Raspberry == "Accel-y") { IMU_Data(Accel, x, y, z); Raspberry_TX(y); }
-            else if(Data_Raspberry == "Accel-z") { IMU_Data(Accel, x, y, z); Raspberry_TX(z); }
-                else if(Data_Raspberry == "Gyro-x") { IMU_Data(Gyro, x, y, z); Raspberry_TX(x); }
-                    else if(Data_Raspberry == "Gyro-y") { IMU_Data(Gyro, x, y, z); Raspberry_TX(y); }
-                        else if(Data_Raspberry == "Gyro-z") { IMU_Data(Gyro, x, y, z); Raspberry_TX(x); }*/
-     // delay(100);
-        Raspberry_TX(Th_Set);
-        Raspberry_RX(Data_Raspberry);
-        Thruster_Setting(Data_Raspberry, TH);
-        Thruster_Speed(TH);
-      }
-      else if(Data_Raspberry == "PowerOFF")
+        }
+            Raspberry_RX(Data_Raspberry);// wait until get the next command              
+            Thruster_Speed(TH);
+
+        Raspberry_TX(TH); //sending the feedback chosen from the thrusters to the raspberry
+
+      
+      else if(strcmp(Data_Raspberry,"PowerOFF")==0)
           {
             Th_PWR = Thruster_PWR(Thruster_OFF);
           }
-          else Raspberry_TX("Wrong Command!");                
-    
+          else Raspberry_TX("Wrong Command!");                   
+     // delay(100);
+        } // end of while loop
+        Raspberry_TX(Th_Set);
+        Raspberry_RX(Data_Raspberry);
+         if(strcmp(Data_Raspberry,"Turn right")==0) { TH[0] = Thruster1; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; }
+        else if(strcmp(Data_Raspberry, "Turn left")==0) { TH[0] = IDLE; TH[1] = Thruster2; TH[2] = IDLE; TH[3] = IDLE; }
+            else if(strcmp(Data_Raspberry, "Surface")==0) { TH[0] = IDLE; TH[1] = IDLE; TH[2] = Thruster3; TH[3] = IDLE;}
+                else if(strcmp(Data_Raspberry, "Descend")==0) { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = Thruster4; }
+                    else if(strcmp(Data_Raspberry, "Go straight")==0) {  TH[0] = Thruster1; TH[1] = Thruster2; TH[2] = IDLE; TH[3] = IDLE; }
+                        else if(strcmp(Data_Raspberry, "Go back")==0) { TH[0] = Reverse1; TH[1] = Reverse2; TH[2] = IDLE; TH[3] = IDLE;  }
+                          else if(strcmp(Data_Raspberry, "Dead zone")==0) { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; }
+        // Thruster_Setting(Data_Raspberry, TH);
+
+
+                   
+     
 }
 
