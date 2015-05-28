77a2e3d1e4654974f68e0170109c6d489759a353
emdort <dorte@my.erau.edu>
2015-05-28 14:13:43 -0400

    Update Agent_lkg_05222015.ino

--- Agent_lkg_05222015/Agent_lkg_05222015.ino
+++ Agent_lkg_05222015/Agent_lkg_05222015.ino
@@ -43,70 +43,43 @@ void loop() {
   
   
   switch(cmd);{
-    
-  case 1:
-      IMU_Data(Accel, x, y, z); Raspberry_TX(x+y+z); 
-      IMU_Data(Gyro, x, y, z); Raspberry_TX(x+y+z);
-    
-   break;
-
-  case 2:
-        if(Th_PWR == 0) { Th_PWR = Thruster_PWR(Thruster_ON); Thruster_Init(); Buzzer_3x500ms(); }
-        Raspberry_TX(Ready);
+        case 1:
+         IMU_Data(Accel, x, y, z); Raspberry_TX(x+y+z); 
+        IMU_Data(Gyro, x, y, z); Raspberry_TX(x+y+z);
+        break;
+        case 2:
+         if(Th_PWR == 0) { Th_PWR = Thruster_PWR(Thruster_ON); Thruster_Init(); Buzzer_3x500ms(); }
+         Raspberry_TX(Ready);
           IMU_Data(Accel, x, y, z); Raspberry_TX(x+y+z); 
           IMU_Data(Gyro, x, y, z); Raspberry_TX(x+y+z); 
         }
-                         
             Thruster_Speed(TH);
-
         Raspberry_TX(TH); //sending the feedback chosen from the thrusters to the raspberry
-
-      break;
-      
-      case 3:
+        break;
+        case 3:
           {
             Th_PWR = Thruster_PWR(Thruster_OFF);
           }
         Raspberry_TX(Th_Set);
-
-        
         break;
-        
-         case 4: { TH[0] = Thruster1; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; }
-         
-         break;
-         
+        case 4: { TH[0] = Thruster1; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; } 
+        break;
         case 5: { TH[0] = IDLE; TH[1] = Thruster2; TH[2] = IDLE; TH[3] = IDLE; }
-        
         break;
-        
-            case 6: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = Thruster3; TH[3] = IDLE;}
-            
-            break;
-            
-                case 7: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = Thruster4; }
-                
-                break;
-                
-                    case 8: {  TH[0] = Thruster1; TH[1] = Thruster2; TH[2] = IDLE; TH[3] = IDLE; }
-                    
-                    break;
-                    
-                        case 9: { TH[0] = Reverse1; TH[1] = Reverse2; TH[2] = IDLE; TH[3] = IDLE;  }
-                        
-                        break;
-                        
-                          case 10: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; }
-                          
-                          break;
-                          
+        case 6: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = Thruster3; TH[3] = IDLE;}
+        break;
+        case 7: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = Thruster4; }
+        break; 
+        case 8: {  TH[0] = Thruster1; TH[1] = Thruster2; TH[2] = IDLE; TH[3] = IDLE; }  
+        break;
+        case 9: { TH[0] = Reverse1; TH[1] = Reverse2; TH[2] = IDLE; TH[3] = IDLE;  }
+        break;
+        case 10: { TH[0] = IDLE; TH[1] = IDLE; TH[2] = IDLE; TH[3] = IDLE; }
+        break;
         // Thruster_Setting(Data_Raspberry, TH);
-        
-        
         case 11:
         Raspberry_TX(Reset);
         digitalWrite (resetPin LOW);
-        
         break;
         }
 
