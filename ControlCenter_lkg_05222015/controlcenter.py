b6d77323a0dc904deb87b2cdafd4ef4a8b56fdb1
ptang0917 <ptang24@gmail.com>
2015-06-01 14:04:22 -0400

    Update controlcenter.py

--- ControlCenter_lkg_05222015/controlcenter.py
+++ ControlCenter_lkg_05222015/controlcenter.py
@@ -3,7 +3,7 @@ import math
 import time
 #from array import array
 # Global variables defined as follows
-port = '/dev/ttyACM1'
+port = '/dev/ttyACM0'
 baud = 57600
 
 IMU = '1'
@@ -20,11 +20,14 @@ Fwd = '11'
 Back = '12'
 targetmag = 89
 difftime = 0
+location = [0,0,0]
+destination = [12,12,5]
 coor =  [0.0,0.0,0.0]
 maxcoor =  [99.0,99.0,99.0]
 i = 0
 
 fob = open('/home/pi/Eco-Dolphin_lkg_05262015/TestSerial/out.txt','w')
+matlab = open('/home/pi/Eco-Dolphin_lkg_05292015/ControlCenter_lkg_05222015/output.m','w')
 prevx = 0
 prevy = 0
 prevz = 0
@@ -92,10 +95,10 @@ def setBoundary(xcoor,ycoor,zcoor):
     maxcoor[0] = xcoor
     maxcoor[1] = ycoor
     maxcoor[2] = zcoor
-    
+
 
 def checkPoint():
-        
+
      #DISCLAIMER: THIS CODE WILL MOST LIKELY NOT WORK SO AS OF NOW THIS IS SOME  PESUDOCODING
         global coor
         global maxcoor
@@ -118,26 +121,27 @@ def checkPoint():
             print 'Target y-coordinate' , maxcoor[1]
 
         if(maxcoor[2] < coor[2]):
-            ser.write(dive)    
+            ser.write(dive)
             print 'Current z-coordinate' , coor[2]
             print 'Target z-coordinate' , maxcoor[2]
         elif(maxcoor[2] > coor[2]):
             ser.write(rise)
             print 'Current z-coordinate' , coor[2]
-            print 'Target z-coordinate' , maxcoor[2] 
+            print 'Target z-coordinate' , maxcoor[2]
 
-        if(maxcoor[0]==coor[0])and(maxcoor[1]==coor[1])and(maxcoor[2]==coor[2]): 
-            ser.write(idle) 
+        if(maxcoor[0]==coor[0])and(maxcoor[1]==coor[1])and(maxcoor[2]==coor[2]):
+            ser.write(idle)
             #Theoretically, the machine will go "IDLE" or cease of all movement should the machine reach its targetted coordinates.
             print 'Target destination reached'
         return None
 
 def getIMU(command):
       ser.write(command)
+      ser.readline()
       responsecmd = ser.readline()
 
       return  responsecmd
-      
+
 
 def getcoordinate(response):
      global prevx
@@ -169,21 +173,21 @@ def getcoordinate(response):
      global accelx
      global accely
      global accelz
-     global fob  
+     global fob
      try:
         print 'testing1'
         fob.write('testing1')
         #responsepwr = ser.readline()
         print 'Agent: ', response
-        fob.write(response + "/n")
+        fob.write(response + '\n')
         if (response == 'Ready'):
              ser.write(IMU)
              responseaccel = ser.readline()
              #ser.write(Gyro)
              responsegyro = ser.readline()
              print 'accel: ', responsegyro
-             
-   
+              
+
              #convert the string response to float
              #xaccel = float(responsex)
              #yaccel = float(responsey)
@@ -192,66 +196,66 @@ def getcoordinate(response):
              #print responsegyro
              fob.write(responseaccel + '\n')
              #fob.write(responsegyro + '\n')
-             
 
-             
+
+
              #print 'Accel x= ', xaccel
              #print 'Accel y= ', yaccel
              #print 'Accel z= ', zaccel
-              
+
              #fob.write('Accel')
              #fob.write('%04d\n' % xaccel)
              #fob.write('%04d\n' % yaccel)
              #fob.write('%04d\n' % zaccel)
 
-             
-	     #tolex += xaccel
+
+             #tolex += xaccel
              #toley += yaccel
              #tolez += zaccel
-             
+
              #diffx = xaccel - prevx
              #diffy = yaccel - prevy
              #diffz = zaccel - prevz
-            
+
              #xaccel = prevx
              #yaccel = prevy
              #zaccel = prevz
-             
+
              #if(i == 10):
                 #avgx = tolex / 10
                 #avgy = toley / 10
-                #avgz = tolez / 10 
+                #avgz = tolez / 10
                 #print 'Total accel  x = ', tolex
                 #print 'Total accel  y = ', toley
                 #print 'Total accel  z = ', tolez
                 #print 'Average accel x = ', avgx
                 #print 'Average accel y = ', avgy
                 #print 'Average accel z = ', avgz
-                
+
                 #fob.write('Total' + "/n")
                 #fob.write('%04d\n' % tolex)
                 #fob.write('%04d\n' % toley)
                 #fob.write('%04d\n' % tolez)
-                
+
                 #fob.write('Average'+ "/n")
                 #fob.write('%04d\n' % avgx)
                 #fob.write('%04d\n' % avgy)
                 #fob.write('%04d\n' % avgz)
 
-                 
+
                 #diffx = accelx - avgx
-	        #diffy = accely - avgy
+                #diffy = accely - avgy
                 #diffz = accelz - avgz
                 #print 'Diff x', diffx
                 #print 'Diff y', diffy
                 #print 'Diff z', diffz
-               
+
                 #fob.write('Diff' + "/n")
                 #fob.write('%03d\n' % diffx)
                 #fob.write('%03d\n' % diffy)
                 #fob.write('%03d\n' % diffz)
-                
-   
+
+
              #anglex = float(responsegyrox)
              #angley = float(responsegyroy)
              #anglez = float(responsegyroz)
@@ -263,8 +267,8 @@ def getcoordinate(response):
              #fob.write(responsegyrox + '\n')
              #fob.write(responsegyroy + '\n')
              #fob.write(responsegyroz + '\n')
-             
-             
+
+
              #ser.write(mag)
              #responsemag = ser.readline()
              #if(targetmag < responsemag):
@@ -283,8 +287,8 @@ def getcoordinate(response):
              # else:
              #hover(targetheading)
 
-             
-             
+
+
             # difftime = time.clock() - prevtime
              #deltax = 0.5*diffx*math.pow(difftime,2)
              #deltay = 0.5*diffy*math.pow(difftime,2)
@@ -295,7 +299,7 @@ def getcoordinate(response):
              #diffanglex = anglex - preanglex
             # diffangley = angley - preangley
             # diffanglez = anglez - preanglez
-        
+
 #find change in time, angle, acceleration, and distance in x,y,z
              # difftime = time.clock() - prevtime
             # diffaccel = math.sqrt((math.pow(diffx,2))+(math.pow(diffy,2))+(math.pow(diffz,2)))
@@ -316,12 +320,12 @@ def getcoordinate(response):
             # print 'Current x: ', currentx
             # print 'Current y: ', currenty
             # print 'Current z: ', currentz
-            
-             print 'IMU Settings received: code success' 
-             
-            # fob.write('Diff Accel') 
+
+             print 'IMU Settings received: code success'
+
+            # fob.write('Diff Accel')
              #fob.write('%05d\n' % diffaccel)
-      
+
             # fob.write('Position')
            #  fob.write('%05d\n' % currentx)
            #  fob.write('%05d\n' % currenty)
@@ -333,73 +337,88 @@ def getcoordinate(response):
 
              return 0 #coor
         else:
-	     print 'Not Ready'
+             print 'Not Ready'
              fob.write('Not Ready' + '\n')
      # ser.write(move)
      except KeyboardInterrupt:
         ser.close()
 
 def resurface():
-     global tol 
+     global tol
      global coor
-   
+
      while coor[2] > tol:
        coor = getcoordinate()
        ser.write(rise)
-      
+
 #main code begins here
 setBoundary(20.0,20.0,10.0)
-while i<=15:
-   ser.write(PwrOn)
-   response = ser.readline()
-   print response
-   if location == destination:
-	while diftime < 15:
-	   ser.write(Hover)
-	ser.write(Idle)
-	#write to sonar
-   else:
-	if response = 'Ready':
-		accel = getIMU(IMU)
-		gyro = getIMU(Gyro)
-	else:
-		print 'Not Ready'
-	
-   
+ser.write(PwrOn)
+ser.readline()
+response = ser.readline()
+print response
+difftime += time.clock() + prevtime
+while 1:
 
-  
-   
-      
-   difftime += time.clock() + prevtime
-   print 'run', i
-   fob.write('run ' + '\n')
-   fob.write('%01d\n' % i) 
-   response = ser.readline()
-   print response   
-  #coor = getcoordinate(response)
-
-  # checkPoint()
-   print 'Current Time' , time.clock()
-   fob.write('Current Time')
-   fob.write('%01d\n' % time.clock())
-  # fob.write(time.clock())
-   print 'Prev Time' , prevtime
-   fob.write('Prev Time')
-   fob.write('%01d\n' % prevtime)
-   print 'Elasped Time' , difftime
-   fob.write('Elasped Time')
-   fob.write('%01d\n' % difftime)
    i+=1
+   if location == destination:
+        while difftime < 15:
+           ser.write(Hover)
+        ser.write(Idle)
+        #write to sonar
+   else:
+        if response == 'Ready':
+                
+                accel = getIMU(IMU)
+                print accel
+                matlab.write(accel + '\n')
+                print 'Current Time' , time.clock()
+                matlab.write('Current Time' + '\n')
+                matlab.write('%01d\n' % time.clock())
+                print 'Prev Time' , prevtime
+                matlab.write('Prev Time' + '\n')
+                matlab.write('%01d\n' % prevtime)
+                print 'Elasped Time' , difftime
+                matlab.write('Elasped Time' + '\n')
+                matlab.write('%01d\n' % difftime)
 
-fob.close()
+                #gyro = getIMU(Gyro)
+                #print gyro
+                
+        else:
+                print 'Not Ready'
+                response = ser.readline()
+
+
+matlab.close()
+# print 'run', i
+# matlab.write('run ' + '\n')
+# matlab.write('%01d\n' % i)
+# response = ser.readline()
+# print response
+#  #coor = getcoordinate(response)
+
+#   # checkPoint()
+# print 'Current Time' , time.clock()
+# fob.write('Current Time')
+# fob.write('%01d\n' % time.clock())
+#   # fob.write(time.clock())
+# print 'Prev Time' , prevtime
+# fob.write('Prev Time')
+# fob.write('%01d\n' % prevtime)
+# print 'Elasped Time' , difftime
+# fob.write('Elasped Time')
+# fob.write('%01d\n' % difftime)
+# i+=1
+
+# fob.close()
   # point = set(coor)
-  # bound = set(maxcoor) 
+  # bound = set(maxcoor)
   # print point & bound
 
-     
+
 # check position against target position (within tolerance)
   # if(((coor[0] > targetx + tol)or(coor[0] < targetx - tol))and((coor[1] > targety + tol)or(coor[1] < targety - tol))and((coor[2] > targetz + tol)or(coor[2] < targetz - tol))):
         # continue
   # else:
        # hover(coor[0], coor[1], coor[2])
-  
