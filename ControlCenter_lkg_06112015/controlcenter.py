786275ed283e7b3231777d42e5e0c6f0affaa9ba
Stacey Joseph-Ellison <ellisos1@my.erau.edu>
2015-06-15 12:32:57 -0400

    SJ-E

--- ControlCenter_lkg_06112015/controlcenter.py
+++ ControlCenter_lkg_06112015/controlcenter.py
@@ -23,42 +23,11 @@ Rise = '9'
 Dive = '10'
 Fwd = '11'
 Back = '12'
-<<<<<<< HEAD
 Reset = '13'
-=======
-targetmag = 89
-difftime = 0
-location = [0,0,0]
-destination = [12,12,5]
-coor =  [0.0,0.0,0.0]
-maxcoor =  [99.0,99.0,99.0]
-#Cp = measure battery
-i = 0
-comma = 0
-fob = open('/home/pi/Eco-Dolphin_lkg_05262015/TestSerial/out.txt','w')
-#matlab = open('/home/pi/Eco-Dolphin/ControlCenter_lkg_06032015/output.m','w')
-prevx = 0
-prevy = 0
-prevz = 0
-preanglex = 0
-preangley = 0
-preanglez = 0
-prevdeltax = 0
-prevdeltay = 0
-prevdeltaz = 0
-prevtime = time.clock()
-
-currentx = 0
-currenty = 0
-currentz = 0
-
-tolex = 0
-toley = 0
-tolez = 0
->>>>>>> origin/master
 
 #tracking variables
 bounds = [20.0,20.0,10.0]
+bed = [0.0,0.0,0.0]
 targetangle = [0.0,0.0,0.0]
 angle = [0.0,0.0,0.0]
 acceleration = [0.0,0.0,0.0]
@@ -151,32 +120,34 @@ def getHeading()
 def hover(x, y, z):
 	global location
 	global tolerance
+	
 	currenttime = time.clock()
     targettime = time.clock()+10
     while(currenttime < targettime):
-		 currenttime+=time.clock()
-         #get current position
+		currenttime+=time.clock()
+        #get current position
 		 
-         if(y < location[1]+tolerance):
+        if(y < location[1]+tolerance):
             ser.write(forward)
-         elif(y > location[1]+tolerance):
+        elif(y > location[1]+tolerance):
             ser.write(backward)
-         else:
+        else:
            ser.write(idle)
 		   
-         if(x < location[0]+tolerance):
+        if(x < location[0]+tolerance):
             ser.write(right)
-         elif(x > location[0]+tolerance):
+        elif(x > location[0]+tolerance):
            ser.write(left)
-         else:
+        else:
            ser.write(idle)
 
-         if(z < location[2]+tolerance):
+        if(z < location[2]+tolerance):
            ser.write(rise)
-         elif(z > location[2]+tolerance):
+        elif(z > location[2]+tolerance):
            ser.write(dive)
-         else:
+        else:
            ser.write(idle)
+		return
 		   
 def setBoundary(xcoor,ycoor,zcoor):
     global bounds
@@ -210,49 +181,43 @@ def checkPoint():
     #compare
     if(bounds[0] < location[0]): #If the value of the x-coordinate given is greater than the target x-coordinate then the machine will move left
         response = cmdAgent(Left)
-        print 'Current x-coordinate' , location[0]
-        print 'Target x-coordinate' , bounds[0]
+        
     elif(bounds[0] > location[0]): #If the value of the x-coordinate given is less than the target x-coordinate then the machine will move right
         response = cmdAgent(Right)
-        print 'Current x-coordinate', location[0]
-        print 'Target x-coordinate' , bounds[0]
+        
 
     if(bounds[1] < location[1]):
         response = cmdAgent(Back) #If the value of the y-coordinate given is greater than the target y-coordinate then the machine will move backward
-        print 'Current y-coordinate' , location[1]
-        print 'Target y-coordinate' , bounds[1]
+        
     elif(bounds[1] > location[1]):
         response = cmdAgent(Forward) #If the value of the y-coordinate given is greater than the target y-coordinate then the machine will move forward
-        print 'Current y-coordinate' , location[1]
-        print 'Target y-coordinate' , bounds[1]
+        
 
     if(bounds[2] < location[2]):
         response = cmdAgent(Dive)
-        print 'Current z-coordinate' , location[2]
-        print 'Target z-coordinate' , bounds[2]
+       
     elif(bounds[2] > location[2]):
         response = cmdAgent(Rise)
-        print 'Current z-coordinate' , location[2]
-        print 'Target z-coordinate' , bounds[2]
+        
 
     if(bounds[0]==location[0])and(bounds[1]==location[1])and(bounds[2]==location[2]):
         response = cmdAgent(Idle)
         #Theoretically, the machine will go "IDLE" or cease of all movement should the machine reach its targetted coordinates.
         print 'Target destination reached'
-    return None
+    return response
 
 def resurface():
-     global tolerance
-     global location
+    global tolerance
+    global location
 
-<<<<<<< HEAD
-     while location[2] > tolerance:
+    while location[2] > tolerance:
        cmdAgent(Rise)
-	   
-       
+	   getPosition()
+	return 0
+
 def proximity ():
-    global coor
-    global n
+    global location
+    global bed
     r = math.sqrt((math.pow((n[0]-coor[0]),2))+(math.pow((n[1]-coor[1]),2))+(math.pow((n[2]-coor[2]),2)))
        if r < tol
           avoid()
@@ -264,44 +229,6 @@ def avoid():
              ser.write(dive)
           else
             ser.write(rise)
-=======
-def tolC():
-	global currentx
-        global currenty
-        global currentz
-        #getCurrent()
-	#goald = math.sqrt((math.pow((destinationx-currentx),2))+(math.pow((destinationy-currenty),2))+(math.pow((destinationz-currentz),2)))
-	#goaltime = (current speed)/goald
-	#Itime = (Cp/math.pow(get.Current(),1.333))
-	#tolCt = Itime- goaltime
-	
-def chkabort():
-	#getCurrent()
-	#tolC()
-	#print getCurrent()
-	if
-	   #tolCt < 0
-	   #resurface()
-	   #set destination[0,0,0]
-	   #print 'abort'
-	else
-	   #continue
-	   
-def chkovride():
-	#ser.readline()
-	if 
-	   #ser.readline = 'override'
-	   #resurface()
-	   #set destination[0,0,0]
-	   #print 'override'
-	else
-	   #continue
-	
-	
-     while coor[2] > tol:
-       coor = getcoordinate()
-       ser.write(rise)
->>>>>>> origin/master
 
 #main code begins here
 setBoundary(bounds)
@@ -322,11 +249,10 @@ while i < 30:
         while difftime < 15:
            ser.write(Hover)
            difftime+=time.clock()
-        ser.write(Idle)
         #write to sonar
 		#retreat
+		resurface()
    else:
-<<<<<<< HEAD
 		#check the state of the agent
         if (response == '') or (response == 'Ready'):
            getPosition()
@@ -341,26 +267,9 @@ while i < 30:
 		   n+=1
 		   if n>15:
 				#average the acceleration
+		   checkPoint()
+		   toHeading()
         else:#add Ready, Abort, Override option
-=======
-        if response == '':
-                
-           accel = getIMU(IMU)
-           time.sleep(.05)
-           print accel
-           endx = accel.find(',')
-           imux = accel[5:endx]
-	   beginy = endx+1
-	   endy = accel[beginy::].find(',')+beginy
-           imuy = accel[beginy:endy]
-	   beginz = endy+1
-           imuz = accel[beginz::]
-           #chkabort()
-           #chkovride()
-           ser.write(Left)
-           ser.readline()
-        else:
->>>>>>> origin/master
                 print 'Not Ready'
                 response = cmdAgent(PwrOn)
 
