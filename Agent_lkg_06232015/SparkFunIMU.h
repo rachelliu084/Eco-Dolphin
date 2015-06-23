37f76bf570c74b957964b4b5aa2b8bd5ee562dd8
ptang0917 <ptang24@gmail.com>
2015-06-23 14:47:10 -0400

    Create SparkFunIMU.h

---
+++ Agent_lkg_06232015/SparkFunIMU.h
@@ -0,0 +1,19 @@
+#ifndef _SPARKFUNIMU_H_
+#define _SPARKFUNIMU_H_
+
+#define SparkFun_IMU_Baud_Rate  57600
+#define IMU_Data_Width  28
+#define Accel  1
+#define Magn  2
+#define Gyro  3  
+
+void SparkFun_IMU_Init();
+
+int Read_Serial_3(char *Data);
+
+void Data_Converter(char *Data, char *x, char *y, char *z);
+
+void IMU_Data(char *Data, int sw);
+
+#endif
+
