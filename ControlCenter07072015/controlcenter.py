a3f341c9e1287a6eee2ce02a19c748400d8c6c45
Stacey Joseph-Ellison <hedstartllc@gmail.com>
2015-07-07 23:28:41 -0400

    Update controlcenter.py

--- ControlCenter07072015/controlcenter.py
+++ ControlCenter07072015/controlcenter.py
@@ -1,3 +1,53 @@
+import serial
+import math
+import time
+import plotly.plotly as py
+from plotly.graph_objs import *
+from array import *
+
+# Global variables defined as follows
+fob = open('/home/pi/Eco-Dolphin1/ControlCenter_lkg_06112015/accel.txt','w')
+fob2 = open('/home/pi/Eco-Dolphin1/ControlCenter_lkg_06112015/ylocation.txt','w')
+port = '/dev/ttyACM0'
+baud = 57600
+response = ''
+
+#commands to agent
+Accel = '1'
+PwrOn = '2'
+PwrOff = '3'
+Idle = '4'
+Right = '5'
+Left = '6'
+Rise = '7'
+Dive = '8'
+Fwd = '9'
+Back = '0'
+
+
+#tracking variables
+bounds = [20.0,20.0,10.0]
+bed = [0.0,0.0,0.0]
+targetangle = [0.0,0.0,0.0]
+angle = [0.0,0.0,0.0]
+heading = [0.0,0.0,0.0]
+acceleration = [0.0,0.0,0.0]
+destination = [12.0,12.0,5.0]
+location = [0.0,0.0,0.0]
+prevlocation = [0.0,0.0,0.0]
+prevaccel=[0.0,0.0,0.0]
+prevangle = [0.0,0.0,0.0]
+prevtime = time.clock()
+Gyro = '7'
+tolerance = 2.0
+elapsetime = 0.0
+summ = [0,0,0]
+diff = [0,0,0]
+diffaccel = [0.0,0.0,0.0]
+diffgyro = [0.0,0.0,0.0]
+xgraphpos = list()
+ygraphpos = list()
+zgraphpos = list()
 #main code begins here
 setBoundary(bounds[0],bounds[1],bounds[2])
 response = cmdAgent(PwrOn)
