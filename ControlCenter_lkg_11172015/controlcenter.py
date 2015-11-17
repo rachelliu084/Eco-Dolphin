c96f8c0dabe7e19887b8aa31aaeb44680b368e8a
Stacey Joseph-Ellison <hedstartllc@gmail.com>
2015-11-17 13:46:50 -0500

    Update controlcenter.py

--- ControlCenter_lkg_11172015/controlcenter.py
+++ ControlCenter_lkg_11172015/controlcenter.py
@@ -10,3 +10,17 @@ fob = open('/home/pi/Eco-Dolphin1/ControlCenter_lkg_06112015/accel.txt','w')
 fob2 = open('/home/pi/Eco-Dolphin1/ControlCenter_lkg_06112015/ylocation.txt','w')
 port = '/dev/ttyACM0'
 baud = 4800
+response = ""
+
+#commands to agent
+Accel = '1'
+PwrOn = '2'
+PwrOff = '3'
+Idle = '4'
+Right = '5'
+Left = '6'
+Rise = '7'
+Dive = '8'
+Fwd = '9'
+Back = '0'
+
